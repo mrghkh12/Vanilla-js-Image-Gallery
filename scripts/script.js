20cdea4d466612ba77149bc42750af8461f202bd
mohammad reza <mrghkh12@gmail.com>
2024-07-22 22:24:38 +0330

    feat: set scrroll left on gallery container

--- scripts/script.js
+++ scripts/script.js
@@ -2,6 +2,20 @@ const $ = document
 
 const galleryContainer = $.querySelector('.gallery')
 
-let backBtn = $.querySelector('.backBtn')
-let nextBtn = $.querySelector('.nextBtn')
+let backBtn = $.querySelector('#backBtn')
+let nextBtn = $.querySelector('#nextBtn')
 
+galleryContainer.addEventListener('wheel' , e =>{
+    e.preventDefault()
+    galleryContainer.scrollLeft += e.deltaY
+})
+
+backBtn.addEventListener('click' , () => {
+    galleryContainer.scrollLeft -= 900
+})
+
+
+nextBtn.addEventListener('click' , () => {
+    console.log('f');
+    galleryContainer.scrollLeft += 900
+})
