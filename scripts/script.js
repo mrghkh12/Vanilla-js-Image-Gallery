1d885d7ac31d73093c80d89c0f48da4245129bd1
mohammad reza <mrghkh12@gmail.com>
2024-07-23 16:36:51 +0330

    feat: creeate scroll Infinity fuction

--- scripts/script.js
+++ scripts/script.js
@@ -6,7 +6,7 @@ let backBtn = $.querySelector('#backBtn')
 let nextBtn = $.querySelector('#nextBtn')
 
 galleryContainer.addEventListener('wheel' , e =>{
-    e.preventDefault()
+    scrollInfinity()
     galleryContainer.scrollLeft += e.deltaY
     galleryContainer.style.scrollBehavior = 'auto'
 })
@@ -14,10 +14,28 @@ galleryContainer.addEventListener('wheel' , e =>{
 backBtn.addEventListener('click' , () => {
     galleryContainer.style.scrollBehavior = 'smooth'
     galleryContainer.scrollLeft -= 900
+    scrollInfinity()
 })
 
 
 nextBtn.addEventListener('click' , () => {
     galleryContainer.style.scrollBehavior = 'smooth'
     galleryContainer.scrollLeft += 900
+    scrollInfinity()
 })
+
+function scrollInfinity(){
+    console.log(galleryContainer.scrollLeft);
+    if(galleryContainer.offsetWidth + galleryContainer.scrollLeft >= galleryContainer.scrollWidth - 100){
+        const rowImg = $.querySelector('.row')
+        let clone = rowImg.cloneNode(true)
+        let parent = rowImg.parentElement
+        console.log(galleryContainer.offsetWidth);
+        parent.appendChild(clone)    
+        if(rowImg.scrollWidth < galleryContainer.scrollLeft){   
+            galleryContainer.scrollLeft -= rowImg.scrollWidth
+            parent.children[0].remove()
+        }
+    }
+
+}
