0f02cb8307e98b057c33290182845557a54e97ba
mohammad reza <mrghkh12@gmail.com>
2024-07-24 07:58:01 +0330

    feat: create infinity scroll back

--- scripts/script.js
+++ scripts/script.js
@@ -6,36 +6,44 @@ let backBtn = $.querySelector('#backBtn')
 let nextBtn = $.querySelector('#nextBtn')
 
 galleryContainer.addEventListener('wheel' , e =>{
-    scrollInfinity()
     galleryContainer.scrollLeft += e.deltaY
     galleryContainer.style.scrollBehavior = 'auto'
+    scrollInfinity()
 })
 
 backBtn.addEventListener('click' , () => {
     galleryContainer.style.scrollBehavior = 'smooth'
     galleryContainer.scrollLeft -= 900
-    scrollInfinity()
-})
 
+})
 
 nextBtn.addEventListener('click' , () => {
     galleryContainer.style.scrollBehavior = 'smooth'
+    console.log(galleryContainer.scrollLeft);
     galleryContainer.scrollLeft += 900
     scrollInfinity()
+    console.log(galleryContainer.scrollLeft);
 })
 
 function scrollInfinity(){
-    console.log(galleryContainer.scrollLeft);
     if(galleryContainer.offsetWidth + galleryContainer.scrollLeft >= galleryContainer.scrollWidth - 100){
         const rowImg = $.querySelector('.row')
         let clone = rowImg.cloneNode(true)
         let parent = rowImg.parentElement
-        console.log(galleryContainer.offsetWidth);
         parent.appendChild(clone)    
         if(rowImg.scrollWidth < galleryContainer.scrollLeft){   
             galleryContainer.scrollLeft -= rowImg.scrollWidth
             parent.children[0].remove()
         }
+    }
+    if(galleryContainer.scrollLeft ==0){
+        const rowImg = $.querySelector('.row')
+        let clone = rowImg.cloneNode(true)
+        let parent = rowImg.parentElement
+        parent.insertBefore(clone, parent.firstChild)  
+        parent.scrollTo(rowImg.scrollWidth,0)
+        parent.lastElementChild.remove()
+        // galleryContainer.scrollLeft = galleryContainer.scrollWidth - rowImg.scrollWidth
     }
 
 }
